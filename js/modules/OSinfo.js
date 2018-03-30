53483526dbe2fe60fb3883b9273d2407cc4df0da
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-30 17:44:10 +0200

    finish OSinfo module

--- js/modules/OSinfo.js
+++ js/modules/OSinfo.js
@@ -1,4 +1,5 @@
 const os = require('os');
+const formatTime = require('./formatTime');
 
 function getOSinfo() {
   let type = os.type();
@@ -10,17 +11,14 @@ function getOSinfo() {
 
   const release = os.release();
   const cpu = os.cpus()[0].model;
-  const uptime = Math.floor(os.uptime() / 60);
   const userInfo = os.userInfo();
+
   console.log('System:', type);
   console.log('Release', release);
   console.log('CPU model:', cpu);
-
-  /* var uptime = os.uptime();
-  console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min'); */
-  console.log('Uptime:', uptime, 'full minutes');
   console.log('User name:', userInfo.username);
   console.log('Home dir:', userInfo.homedir);
+  formatTime.print();
 }
 
 exports.print = getOSinfo;
